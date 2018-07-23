93ffc23e45a129e66d34be55033fe407f33bf384
Kevc2003 <40496216+Kevc2003@users.noreply.github.com>
2018-07-23 17:26:17 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,49 @@
+const Discord = require ('discord.js');
+
+const bot = new Discord.Client ();
+const prefix = "/";
+
+
+bot.on('ready', () => {
+    bot.user.setPresence({ game: { name: '[/help]'}});
+    console.log("Bot Prêt !");
+});
+
+bot.login('NDU4NTU0ODA0ODMxMjU2NTc2.DgqZQA.PcSjGpFksVpdPyhsvkRjly2qDa0');
+
+bot.on('message', message => {
+    if (message.content === prefix + "ping"){
+        message.channel.send("pong :joy:");
+        console.log('ping pong');
+    }
+    
+    if (message.content === prefix + "help"){
+        var help_embed = new Discord.RichEmbed()
+        .setColor('#F3009A')
+        .addField("Commandes du Bot", " -/help pour afficher les commandes \n-/ping  :wink: \n-/infos Pour les infos du serveur")
+        message.channel.send(help_embed);
+        console.log("help Demandé")
+    }
+
+    if (message.content === prefix + "infos"){
+        var help_embed = new Discord.RichEmbed()
+        .setDescription("Infos du serveur")
+        .addField("Nom du discord", message.guild.name)
+        .addField("crée le", message.guild.createdAt)
+        .addField("Tu a rejoin le", message.member.joinedAt)
+        .addField("Utilisateur sur le Discord", message.guild.memberCount)  
+        .setColor("#F3009A") 
+    message.channel.send(help_embed)
+    }
+
+    });
+
+    bot.on('guildMemberAdd', member => {
+        member.guild.channels.get("456903847521288213").send(":tada:Bonjour <@!" + member.id + "> et bienvenue sur **Aliria**, ammuse toi bien !:wink: ");
+        console.log("Bienvenue " + member.user.username)
+    });
+       
+    bot.on('guildMemberAdd', member => {
+        var role = member.guild.roles.find('name', 'Membres');
+        member.addRole(role)
+    })
